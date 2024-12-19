580d0d480f3119b28773ce7f37f7fa199c803526
Lenar <tyttrost1@gmail.com>
2024-12-19 12:13:06 +0500

    lab9

---
+++ labo9/main.py
@@ -0,0 +1,16 @@
+def generator(pos1, pos2):
+    for i in pos1:
+        for j in pos2:
+            result=i+j
+            yield result
+
+
+
+
+pos=input("введите последоватльность 1: ")
+pos1=set(pos)
+pos=input("введите последоватльность 2: ")
+pos2=set(pos)
+otvet=generator(pos1,pos2)
+for i in otvet:
+    print(i)
